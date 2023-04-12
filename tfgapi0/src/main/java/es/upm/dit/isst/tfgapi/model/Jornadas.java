9d9f268a94f23977dc7002840e0d33b4350ee08c
irbalao <123631091+irbalao@users.noreply.github.com>
2023-04-12 19:14:23 +0200

    Add files via upload

--- tfgapi0/src/main/java/es/upm/dit/isst/tfgapi/model/Jornadas.java
+++ tfgapi0/src/main/java/es/upm/dit/isst/tfgapi/model/Jornadas.java
@@ -1,6 +1,8 @@
 package es.upm.dit.isst.tfgapi.model;
 
 import javax.persistence.*;
+
+import java.sql.Time;
 import java.util.Date;
 
 
@@ -16,11 +18,13 @@ public class Jornadas {
     @Column(name = "idEmpleado", length = 4)
     private String idEmpleado;
     @Id
+    @Temporal(TemporalType.DATE)
+    @Column(name = "fecha", nullable = true, columnDefinition = "DATE DEFAULT CURRENT_DATE")
     private Date fecha;
-    private Date hora_entrada;
-    private Date hora_salida;
-    private Date entrada_teorica;
-    private Date salida_teorica;
+    private Time hora_entrada;
+    private Time hora_salida;
+    private Time entrada_teorica;
+    private Time salida_teorica;
     private Integer minutos_trabajados;
     private Integer minutos_teoricos;
     private Integer saldo;
@@ -47,25 +51,25 @@ public class Jornadas {
     public Date getHora_entrada() {
         return hora_entrada;
     }
-    public void setHora_entrada(Date hora_entrada) {
+    public void setHora_entrada(Time hora_entrada) {
         this.hora_entrada = hora_entrada;
     }
     public Date getHora_salida() {
         return hora_salida;
     }
-    public void setHora_salida(Date hora_salida) {
+    public void setHora_salida(Time hora_salida) {
         this.hora_salida = hora_salida;
     }
     public Date getEntrada_teorica() {
         return entrada_teorica;
     }
-    public void setEntrada_teorica(Date entrada_teorica) {
+    public void setEntrada_teorica(Time entrada_teorica) {
         this.entrada_teorica = entrada_teorica;
     }
     public Date getSalida_teorica() {
         return salida_teorica;
     }
-    public void setSalida_teorica(Date salida_teorica) {
+    public void setSalida_teorica(Time salida_teorica) {
         this.salida_teorica = salida_teorica;
     }
     public Integer getMinutos_trabajados() {
