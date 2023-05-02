ceb5f1e9a4e81c68c9f02ef35c7805b97fcec36f
Chema <chematejedor2098@gmail.com>
2023-05-02 20:42:47 +0200

    Jornadas

--- tfgapi0/src/main/java/es/upm/dit/isst/tfgapi/model/Jornadas.java
+++ tfgapi0/src/main/java/es/upm/dit/isst/tfgapi/model/Jornadas.java
@@ -1,93 +1,85 @@
 package es.upm.dit.isst.tfgapi.model;
 
 import javax.persistence.*;
+import java.time.LocalTime;
 
-import java.sql.Time;
-import java.util.Date;
-
-
-
-
+import javax.persistence.EmbeddedId;
 
 @Entity
-@Table(name = "Jornadas")
-@IdClass(value = jornadasPK.class)
 public class Jornadas {
 
-    @Id
-    @Column(name = "idEmpleado", length = 4)
-    private String idEmpleado;
-    @Id
-    @Temporal(TemporalType.DATE)
-    @Column(name = "fecha", nullable = true, columnDefinition = "DATE DEFAULT CURRENT_DATE")
-    private Date fecha;
-    private Time hora_entrada;
-    private Time hora_salida;
-    private Time entrada_teorica;
-    private Time salida_teorica;
-    private Integer minutos_trabajados;
-    private Integer minutos_teoricos;
-    private Integer saldo;
+    @EmbeddedId
+    private jornadasPK clave;
+    private LocalTime hora_entrada;
+    private LocalTime hora_salida;
+    private LocalTime entrada_teorica;
+    private LocalTime salida_teorica;
+    private int minutos_trabajados;
+    private int minutos_teoricos;
+    private int saldo;
     @Column(name = "incidencia", length = 3)
     private String incidencia;
     @Column(name = "estado", length = 2)
     private String estado;
     private String notas;
 
+    public Jornadas() {
 
+    }
 
-    public String getIdEmpleado() {
-        return idEmpleado;
+    public jornadasPK getClave() {
+        return clave;
     }
-    public void setIdEmpleado(String idEmpleado) {
-        this.idEmpleado = idEmpleado;
+    public void setClave(jornadasPK clave) {
+        this.clave = clave;
     }
-    public Date getFecha() {
-        return fecha;
-    }
-    public void setFecha(Date fecha) {
-        this.fecha = fecha;
-    }
-    public Date getHora_entrada() {
+    public LocalTime getHora_entrada() {
         return hora_entrada;
     }
-    public void setHora_entrada(Time hora_entrada) {
+
+    public void setHora_entrada(LocalTime hora_entrada) {
         this.hora_entrada = hora_entrada;
     }
-    public Date getHora_salida() {
+    public LocalTime getHora_salida() {
         return hora_salida;
     }
-    public void setHora_salida(Time hora_salida) {
+
+    public void setHora_salida(LocalTime hora_salida) {
         this.hora_salida = hora_salida;
     }
-    public Date getEntrada_teorica() {
+    public LocalTime getEntrada_teorica() {
         return entrada_teorica;
     }
-    public void setEntrada_teorica(Time entrada_teorica) {
+
+    public void setEntrada_teorica(LocalTime entrada_teorica) {
         this.entrada_teorica = entrada_teorica;
     }
-    public Date getSalida_teorica() {
+    public LocalTime getSalida_teorica() {
         return salida_teorica;
     }
-    public void setSalida_teorica(Time salida_teorica) {
+
+    public void setSalida_teorica(LocalTime salida_teorica) {
         this.salida_teorica = salida_teorica;
     }
-    public Integer getMinutos_trabajados() {
+    public int getMinutos_trabajados() {
         return minutos_trabajados;
     }
-    public void setMinutos_trabajados(Integer minutos_trabajados) {
+
+    public void setMinutos_trabajados(int minutos_trabajados) {
         this.minutos_trabajados = minutos_trabajados;
     }
-    public Integer getMinutos_teoricos() {
+    public int getMinutos_teoricos() {
         return minutos_teoricos;
     }
-    public void setMinutos_teoricos(Integer minutos_teoricos) {
+
+    public void setMinutos_teoricos(int minutos_teoricos) {
         this.minutos_teoricos = minutos_teoricos;
     }
-    public Integer getSaldo() {
+    public int getSaldo() {
         return saldo;
     }
-    public void setSaldo(Integer saldo) {
+
+    public void setSaldo(int saldo) {
         this.saldo = saldo;
     }
     public String getIncidencia() {
